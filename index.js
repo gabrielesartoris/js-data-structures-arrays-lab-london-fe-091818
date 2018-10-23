fbdc24b21caa7344e4695b108efa896361ae55fb
Gabriele Sartoris <sartoris.gabriele@gmail.com>
2018-10-23 18:30:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyPrependDriver(name) {
 }
 
 function destructivelyRemoveLastDriver() {
-  drivers.pop('Milo');
+  drivers.pop('Garfield');
 }
 
 function destructivelyRemoveFirstDriver() {
